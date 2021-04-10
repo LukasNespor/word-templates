556db7dc57fac14dd3bf6ec3eb415f62f8865c80
Nešpor Lukáš <lukasnespor@gmail.com>
2021-04-10 21:20:09 +0200

    Refactor GetToken

--- server/Func/GetToken.cs
+++ server/Func/GetToken.cs
@@ -12,72 +12,21 @@ namespace LNE.GetSAS
     public static class GetToken
     {
         [FunctionName(nameof(GetToken))]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "token")]HttpRequest req)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "token")] HttpRequest req)
         {
             var container = BlobService.GetContainer(Constants.TemplatesContainerName);
             var token = GetContainerSASToken(container, SharedAccessBlobPermissions.Create);
-            return new OkObjectResult(new
-            {
-                Token = token,
-                Host = container.ServiceClient.StorageUri.PrimaryUri
-            });
+            return new OkObjectResult($"BlobEndpoint={container.ServiceClient.StorageUri.PrimaryUri};SharedAccessSignature={token}");
         }
 
-        public static string GetBlobSASToken(CloudBlobContainer container, string blobName, SharedAccessBlobPermissions permissions, string policyName = null)
+        public static string GetContainerSASToken(CloudBlobContainer container, SharedAccessBlobPermissions permissions)
         {
-            string sasBlobToken;
-
-            // Get a reference to a blob within the container.
-            // Note that the blob may not exist yet, but a SAS can still be created for it.
-            CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
-
-            if (policyName == null)
-            {
-                var adHocSas = CreateAdHocSasPolicy(permissions);
-
-                // Generate the shared access signature on the blob, setting the constraints directly on the signature.
-                sasBlobToken = blob.GetSharedAccessSignature(adHocSas);
-            }
-            else
-            {
-                // Generate the shared access signature on the blob. In this case, all of the constraints for the
-                // shared access signature are specified on the container's stored access policy.
-                sasBlobToken = blob.GetSharedAccessSignature(null, policyName);
-            }
-
-            return sasBlobToken;
-        }
-
-        public static string GetContainerSASToken(CloudBlobContainer container, SharedAccessBlobPermissions permissions, string storedPolicyName = null)
-        {
-            string sasContainerToken;
-
-            // If no stored policy is specified, create a new access policy and define its constraints.
-            if (storedPolicyName == null)
-            {
-                var adHocSas = CreateAdHocSasPolicy(permissions);
-
-                // Generate the shared access signature on the container, setting the constraints directly on the signature.
-                sasContainerToken = container.GetSharedAccessSignature(adHocSas, null);
-            }
-            else
-            {
-                // Generate the shared access signature on the container. In this case, all of the constraints for the
-                // shared access signature are specified on the stored access policy, which is provided by name.
-                // It is also possible to specify some constraints on an ad-hoc SAS and others on the stored access policy.
-                // However, a constraint must be specified on one or the other; it cannot be specified on both.
-                sasContainerToken = container.GetSharedAccessSignature(null, storedPolicyName);
-            }
-
-            return sasContainerToken;
+            var adHocSas = CreateAdHocSasPolicy(permissions);
+            return container.GetSharedAccessSignature(adHocSas, null);
         }
 
         private static SharedAccessBlobPolicy CreateAdHocSasPolicy(SharedAccessBlobPermissions permissions)
         {
-            // Create a new access policy and define its constraints.
-            // Note that the SharedAccessBlobPolicy class is used both to define the parameters of an ad-hoc SAS, and 
-            // to construct a shared access policy that is saved to the container's shared access policies. 
-
             return new SharedAccessBlobPolicy()
             {
                 // Set start time to five minutes before now to avoid clock skew.
